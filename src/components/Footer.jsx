99ab29256d4e2a731bd4dc05e4dec186e3dc448c
umartils <umartilmisani48@gmail.com>
2026-04-10 23:34:40 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,23 +14,30 @@ const socialLinks = [
 export default function Footer() {
   return (
     <div className="mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center">
-        <h1 className="text-2xl font-bold">
-        Portofolio
-        </h1>
-        <div className="flex items-center gap-6">
+      <h1 className="text-2xl font-bold">Portofolio</h1>
+      {/* <div className="flex items-center gap-6">
             {navLinks.map((link) => (
               <a key={link.href} href={link.href}>
                 {link.label}
               </a>
             ))}
-        </div>
-        <div className="flex items-center gap-3">
-            {socialLinks.map((link) => (
-              <a key={link.icon} href={link.href} target="_blank" rel="noopener noreferrer" className="text-zinc-400 hover:text-violet-500 transition-colors">
-                <i className={`${link.icon} ri-2x`}></i>
-              </a>
-            ))}
-        </div>
+        </div> */}
+      <div className="flex items-center gap-3">
+        {socialLinks.map((link) => (
+          <a
+            key={link.icon}
+            href={link.href}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-zinc-400 hover:text-violet-500 transition-colors"
+          >
+            <i className={`${link.icon} ri-2x`}></i>
+          </a>
+        ))}
+      </div>
+      <p className="text-xs text-zinc-400">
+        © {new Date().getFullYear()} Umar Tilmisani. All rights reserved.
+      </p>
     </div>
-  )
+  );
 }
